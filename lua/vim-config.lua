3509d0d15b0baaffeea003087f201726dd5fb4d3
Matthew Sells <matthew.sells@proton.me>
2024-06-29 11:20:06 -0500

    add base linting/formatting

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -3,8 +3,9 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 
 vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
-vim.keymap.set("n", "<leader>h", "<C-w><left>")
+vim.keymap.set({ "n", "i" }, "<leader>v", ":vsplit<CR>")
+vim.keymap.set("n", "<leader>a", "<C-w><left>")
 vim.keymap.set("n", "<leader>l", "<C-w><right>")
 vim.keymap.set("n", "<BS>", "ciw")
-vim.keymap.set({ "n", "i" }, "<leader>v", ":vsplit<CR>")
