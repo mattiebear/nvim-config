8d0a07c5855928022bc0e5ffb20cb359502ae983
Matthew Sells <matthew.sells@proton.me>
2024-06-30 20:42:35 -0500

    add debug config

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,51 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" },
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+		dapui.setup()
+
+		dap.adapters.mix_task = {
+			type = "executable",
+			command = "/Users/matt/dev/elixir-ls-build/debugger.sh",
+			args = {},
+		}
+
+		dap.configurations.elixir = {
+			{
+				type = "mix_task",
+				name = "mix test",
+				task = "test",
+				taskArgs = { "--trace" },
+				request = "launch",
+				startApps = true,
+				projectDir = "${workspaceFolder}",
+				requireFiles = {
+					"test/**/test_helper.exs",
+					"test/**/*_test.exs",
+				},
+			},
+		}
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+	end,
+}
